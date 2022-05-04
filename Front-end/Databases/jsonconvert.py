c03288d6cf9446d2ddd065442e384f84d87ef386
Gia Khanh Le <493341@student.saxion.nl>
2022-05-04 02:51:56 +0200

    add data to draw graphs for WebUI

---
+++ Front-end/Databases/jsonconvert.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import json
+
+connection = mysql.connector.connect(user = 'root', password = '05022002', host = 'localhost', database = 'world')
+if connection: 
+    print("Connection successfully")
+else:
+    print("Connection Not Established")
+
+class create_dict(dict): 
+    # __init__ function 
+    def __init__(self): 
+        self = dict() 
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value
+
+mydict = create_dict()
+select_table = """SELECT * FROM city LIMIT 5"""
+cursor = connection.cursor()
+cursor.execute(select_table)
+result = cursor.fetchall()
+
+for row in result:
+    mydict.add(row[0],({"name":row[1],"countrycode":row[2],"district":row[3],"population":row[4]}))
+stud_json = json.dumps(mydict, indent=2, sort_keys=True)
+
+with open("./Front-end/Databases/listofcities.json", "w") as f:
+    f.write(stud_json)
+
+print(stud_json)

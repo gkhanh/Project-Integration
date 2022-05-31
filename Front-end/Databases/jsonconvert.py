044dc61eed0c518f13d5c7b5d4576f740c2f3527
Gia Khanh Le <493341@student.saxion.nl>
2022-05-31 09:35:56 +0200

    Fixing error and code clean up

--- Front-end/Databases/jsonconvert.py
+++ Front-end/Databases/jsonconvert.py
@@ -1,36 +1,21 @@
 import mysql.connector
 import json
 
+#connection = mysql.connector.connect(user = 'id18940854_group7', password = '@Projectintegrationg7', host = 'localhost', database = 'id18940854_esp32_data')
 connection = mysql.connector.connect(user = 'root', password = '05022002', host = 'localhost', database = 'world')
 if connection: 
     print("Connection successfully")
 else:
     print("Connection Not Established")
 
-#class create_dict(dict): 
-    # __init__ function 
-    #def __init__(self): 
-        #self = dict() 
-    # Function to add key:value 
-    #def add(self, key, value): 
-        #self[key] = value
-
-#mydict = create_dict()
-#myArr = [mydict]
-
-#
 select_table = "SELECT * FROM city LIMIT 8"
 cursor = connection.cursor(dictionary=True)
 cursor.execute(select_table)
 result = cursor.fetchall()
 jsonResult = {"ListOfThings": result}
-
 #for row in result:
     #mydict.add(row[0],{"name":row[1],"countrycode":row[2],"district":row[3],"population":row[4]})
-    
 stud_json = json.dumps(jsonResult, indent=2, sort_keys=True)
-
-with open("./Front-end/Databases/listofcities.json", "w") as f:
+with open("./Databases/listofcities.json", "w") as f:
     f.write(stud_json)
-
 print(stud_json)

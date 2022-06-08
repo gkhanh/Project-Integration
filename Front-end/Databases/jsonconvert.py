c1c3f4bad6cbabc83d0daa0668748ffb7031eec4
Gia Khanh Le <493341@student.saxion.nl>
2022-06-08 17:02:15 +0200

    code clean up and draw

--- Front-end/Databases/jsonconvert.py
+++ Front-end/Databases/jsonconvert.py
@@ -1,14 +1,14 @@
 import mysql.connector
 import json
 
-#connection = mysql.connector.connect(user = 'id18940854_group7', password = '@Projectintegrationg7', host = 'localhost', database = 'id18940854_esp32_data')
-connection = mysql.connector.connect(user = 'root', password = '05022002', host = 'localhost', database = 'world')
+#connection = mysql.connector.connect(user = 'id19022493_root', password = '@Projectintg7', host = 'localhost', database = 'id19022493_esp32')
+connection = mysql.connector.connect(user = 'root', password = '05022002', host = 'localhost', database = 'esp32data')
 if connection: 
     print("Connection successfully")
 else:
     print("Connection Not Established")
 
-select_table = "SELECT * FROM city LIMIT 8"
+select_table = "SELECT * FROM led_status LIMIT 8"
 cursor = connection.cursor(dictionary=True)
 cursor.execute(select_table)
 result = cursor.fetchall()
@@ -16,6 +16,6 @@ jsonResult = {"ListOfThings": result}
 #for row in result:
     #mydict.add(row[0],{"name":row[1],"countrycode":row[2],"district":row[3],"population":row[4]})
 stud_json = json.dumps(jsonResult, indent=2, sort_keys=True)
-with open("./Databases/listofcities.json", "w") as f:
+with open("./Databases/test.json", "w") as f:
     f.write(stud_json)
 print(stud_json)

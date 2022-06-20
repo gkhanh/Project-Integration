012dc606d883fbba0062e60320987d17059dd475
Gia Khanh Le <493341@student.saxion.nl>
2022-06-20 09:01:06 +0200

    update map to webserver

--- Front-end/public/src/pagemap.js
+++ Front-end/public/src/pagemap.js
@@ -1,17 +1,23 @@
+var longtitude;
+var latitude;
+
 // Initialize and add the map
 function initMap() {
     // The location of ESP32
-    const esp32Pos = { lat: 35, lng: 21.031 };
-    // The map, centered at Uluru
+    const esp32Pos = { lat: 52.2207, lng: 6.8866 };
+    // The map, centered at ESP32
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: esp32Pos,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at ESP32
     const marker = new google.maps.Marker({
       position: esp32Pos,
       map: map,
     });
   }
   
+function getLatitude(){
+
+}
   window.initMap = initMap;
